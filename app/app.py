98e9d7d900c51ec0ae698d999f4daae59b050ac1
prdeck <punidramesh@gmail.com>
2021-04-29 21:40:51 +0530

    added a flask server for OAuth

--- app/app.py
+++ app/app.py
@@ -1,36 +1,50 @@
-from flask import Flask, jsonify, request, render_template
-from flask_restful import Resource, Api
+from flask import Flask, jsonify, request, render_template, abort,make_response
 import requests, os
+from dotenv import load_dotenv
 
 PORT = os.getenv('PORT')
 REDIRECT_URL = os.getenv('REDIRECT_URL');
-STATE = os.getenv()
+STATE = os.getenv('STATE')
 CLIENTID = os.getenv('CLIENTID')
 CLIENT_SECRET = os.getenv('CLIENT_SECRET')
 
-app = Flask(__name__)
-api = Api(app)
-access_token = ''
+app = Flask(__name__, template_folder='templates')
+code = ''
+flag = 0
+
+@app.errorhandler(404)
+def not_found(error):
+	return make_response(jsonify({'error': 'Not found'}), 404)
 
 @app.route('/', methods = ['GET'])
 def home():
-    if request.method == 'GET':
-        return jsonify({'message': 'Hello'})
+	if request.method == 'GET':
+		return jsonify({'message': 'Hello'})
 
 @app.route('/auth/redirect')
 def getResponse():
-	code = request.args.get("code")
+	code = request.args.get('code')
 	if code != None:
-		ACCESS_URI = f"https://www.coinbase.com/oauth/token?grant_type=authorization_code&code={code}&client_id={clientID}&client_secret={secret}&redirect_uri={REDIRECT_URL}"
-		at = requests.post(ACCESS_URI).json()
-		access_token = at['access_token']
-		return render_template('index.html')
+		flag = 1
+		return render_template('inprogress.html')
 	else:
-			return "Invalid token"
+		abort(404)
 
-class Token(Resource):
-    def get(self):
-        return jsonify({'token': access_token})
+@app.route('/api/code', methods=['GET'])
+def returncode():
+	print(code)
+	return jsonify({'code': code})	
+	
+@app.route('/api/status', methods=['GET'])
+def userStatus():
+	return jsonify({'flag': flag})	
 
-api.add_resource(Token, 'auth/token')
-
+@app.route('/api/auth/status', methods=['POST'])
+def status():
+	status = request.args.get('status').json()
+	if status['status'] == 'success':
+		return render_template('complete.html')
+	elif status['status'] == 'invalidAT':
+		return render_template('error.html', message = 'Authentication failed, Invalid code received from Coinbase')
+	elif status['status'] == 'invalidC':
+		return render_template('error.html', message = 'Authenication failed, access token was not received from Coinbase')
